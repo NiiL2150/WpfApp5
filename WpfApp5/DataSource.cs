75fe6862f4916f7cba9fda00653e34aac605c91f
NiiL2150 <nail.shamsudinov@gmail.com>
2021-07-13 19:53:51 +0400

    Add project files.

---
+++ WpfApp5/DataSource.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace WpfApp5
+{
+    class DataSource : INotifyPropertyChanged
+    {
+        private int value = 0;
+        public int Value
+        {
+            get => value;
+            set
+            {
+                if (!this.value.Equals(value))
+                {
+                    this.value = value;
+                    OnChange(new PropertyChangedEventArgs(nameof(this.value)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnChange(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+    }
+}
---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(500) };
+            timer.Tick += TimerTick;
+            DataContext = new DataSource();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            ((DataSource)DataContext).Value++;
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            tickLabel.Content = ((DataSource)DataContext).Value;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!timer.IsEnabled)
+            {
+                timer.Start();
+            }
+            else
+            {
+                timer.Stop();
+            }
+        }
+    }
+}
